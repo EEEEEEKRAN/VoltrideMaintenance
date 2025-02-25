63f16f579ad890872d11c389dac259f733d584e8
Arthur serenne <arthur.serenne@gmail.com>
2025-02-25 11:28:01 +0100

    page user non connecté

--- frontend/src/components/Accueil.tsx
+++ frontend/src/components/Accueil.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Paper } from '@mui/material';
+import { Box, Typography, Paper, Button, TextField } from '@mui/material';
 import { keyframes } from '@mui/system';
 
 
@@ -16,39 +16,173 @@ const fadeIn = keyframes`
 
 const Accueil: React.FC = () => {
   return (
-    <Box
-      sx={{
-        height: 'calc(100vh - 64px)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background: 'linear-gradient(135deg, #4CAF50 0%, #2E7D32 100%)',
-      }}
-    >
-      <Paper
-        elevation={8}
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      
+      {/* Section 1 - Hero */}
+      <Box
         sx={{
-          padding: 4,
-          borderRadius: 4,
-          animation: `${fadeIn} 1s ease-out`,
-          background: 'rgba(255, 255, 255, 0.9)',
-          backdropFilter: 'blur(10px)',
-          boxShadow: '0 8px 32px rgba(0, 0, 0, 0.1)',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundImage: 'url(../../images/home.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
       >
-        <Typography
-          variant="h2"
-          component="h1"
+        <Paper
+          elevation={8}
           sx={{
-            color: '#1B5E20',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
+            padding: 4,
+            borderRadius: 4,
+            animation: `${fadeIn} 1s ease-out`,
+            background: 'rgba(255, 255, 255, 0.7)',
+            backdropFilter: 'blur(10px)',
+            boxShadow: '0 8px 32px rgba(0, 0, 0, 0.1)',
           }}
         >
-          Bienvenue sur notre application !
+          <Typography
+            variant="h2"
+            component="h1"
+            sx={{
+              color: '#1B5E20',
+              fontWeight: 'bold',
+              textAlign: 'center',
+              textShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
+            }}
+          >
+            Bienvenue sur notre application !
+          </Typography>
+        </Paper>
+      </Box>
+
+      {/* Section 2 - À propos */}
+      <Box
+        sx={{
+          py: 10,
+          px: 4,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#f5f5f5',
+          flexDirection: { xs: 'column', md: 'row' }, // Colonne sur mobile, ligne sur desktop
+          gap: 4, // Espacement entre l'image et le texte
+        }}
+      >
+        {/* Image à gauche */}
+        <Box sx={{ width: { xs: '100%', md: '50%' }, textAlign: 'justify' }}>
+          <Typography variant="h3" sx={{ fontWeight: 'bold', color: '#1B5E20', textAlign: 'center' }}>
+            À propos de nous
+          </Typography>
+          <Typography variant="body1" sx={{ mt: 2, p: 8 }}>
+          VoltRide révolutionne la mobilité urbaine en offrant une solution de location de scooters 100% en ligne. Grâce à notre plateforme intuitive, louez un scooter en quelques clics, trouvez un véhicule disponible près de chez vous et profitez d’un transport rapide, économique et écologique. Plus besoin de paperasse ni d’attente, tout se fait via l’application pour une expérience fluide et sécurisée. 🚀🛵
+          </Typography>
+          <Button>
+          Prêt à rouler ?
+          </Button>
+        </Box>
+        
+        {/* Texte à droite */}
+        <Box
+          component="img"
+          src="../../images/scoot.jpeg"
+          alt="À propos de nous"
+          sx={{
+            width: { xs: '100%', md: '40%' }, // 100% sur mobile, 40% sur desktop
+            borderRadius: 2,
+            boxShadow: '0 4px 10px rgba(0,0,0,0.2)',
+          }}
+        />
+      </Box>
+
+      {/* Section 3 - Nos Services */}
+      <Box
+        sx={{
+          py: 10,
+          px: 4,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundImage: 'linear-gradient(to right, #4CAF50, #81C784)',
+          color: 'white',
+          flexDirection: { xs: 'column', md: 'row' }, // Stack en colonne sur mobile, en ligne sur desktop
+          gap: 4, // Espacement entre les éléments
+        }}
+      >
+        {/* Image à gauche */}
+        <Box
+          component="img"
+          src="../../images/scoot.jpeg"
+          alt="Nos services"
+          sx={{
+            width: { xs: '100%', md: '40%' }, // Image prend toute la largeur sur mobile, 40% sur desktop
+            borderRadius: 2,
+            boxShadow: '0 4px 10px rgba(0,0,0,0.2)',
+          }}
+        />
+
+        {/* Texte à droite */}
+        <Box sx={{ width: { xs: '100%', md: '50%' }, textAlign: 'center' }}>
+          <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+            Un problème ? Une solution !
+          </Typography>
+          <Typography variant="body1" sx={{ mt: 2 }}>
+            N'hésitez pas à reporter votre problème via ce formualire.
+          </Typography>
+          <Box sx={{ width: { xs: '100%', md: '50%' }, textAlign: 'left' }}>
+            
+            {/* Formulaire */}
+            <Box component="form" sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 2 }}>
+              <TextField 
+                label="Nom" 
+                variant="outlined" 
+                fullWidth 
+                sx={{ backgroundColor: 'white', borderRadius: 1 }}
+              />
+              <TextField 
+                label="Email" 
+                type="email" 
+                variant="outlined" 
+                fullWidth 
+                sx={{ backgroundColor: 'white', borderRadius: 1 }}
+              />
+              <TextField 
+                label="Message" 
+                variant="outlined" 
+                multiline 
+                rows={4} 
+                fullWidth 
+                sx={{ backgroundColor: 'white', borderRadius: 1 }}
+              />
+              <Button 
+                type="submit" 
+                variant="contained" 
+                sx={{ backgroundColor: 'white', color: '#4CAF50' }}
+              >
+                Envoyer
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+      {/* Section 4 - Contact */}
+      <Box
+        sx={{
+          py: 10,
+          px: 4,
+          textAlign: 'center',
+          backgroundColor: '#263238',
+          color: 'white',
+        }}
+      >
+        <Typography variant="h3" sx={{ fontWeight: 'bold' }}>Contactez-nous</Typography>
+        <Typography variant="body1" sx={{ maxWidth: '800px', mx: 'auto', mt: 2 }}>
+          Nous serions ravis d'échanger avec vous ! N'hésitez pas à nous contacter pour toute question.
         </Typography>
-      </Paper>
+      </Box>
+
     </Box>
   );
 };
---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Accueil from '../components/Accueil';
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <p>Utilisateur connecté</p>
+    </>
+  );
+};
+
+export default HomePage;
